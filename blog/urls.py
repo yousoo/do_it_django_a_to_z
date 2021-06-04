3b2e5d62e1708cb9a44d43a62ea60b429dc38b60
yousoo <jyousoo@hanmail.net>
2021-06-04 20:48:33 +0900

    포스트 목록 페이지에 대표 이미지 출력하기

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
     path('', views.index),
-    path('<int:pk>/', views.single_post_page),
+    #path('<int:pk>/', views.single_post_page),
+    path('<int:pk>/', views.post_detail),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,12 @@ def index(request):
     posts = Post.objects.all().order_by('-pk')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+"""
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
     return render(request, 'blog/single_post_page.html', {'post': post})
+"""
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
