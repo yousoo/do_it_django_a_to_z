3a5e269e103b8b665c7c27ae3c633bd68a614471
yousoo <jyousoo@hanmail.net>
2021-06-04 13:11:37 +0900

    포스트 상세페이지

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from .models import Post
 # Create your views here.
 def index(request):
     posts = Post.objects.all().order_by('-pk')
-    return render(request, 'blog/index.html',{'posts': posts})
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
--- do_it_django_a_to_z/settings.py
+++ do_it_django_a_to_z/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
+    'single_pages',
 ]
 
 MIDDLEWARE = [
@@ -104,15 +106,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
